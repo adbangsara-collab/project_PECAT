d23ee2966296745c39f8dabba744ca060456ed28
jgmacasieb-design <jgmacasieb@carc.pshs.edu.ph>
2026-03-06 15:28:24 +0800

    Update CS2_Project.py

--- CS2_Project.py
+++ CS2_Project.py
@@ -3,6 +3,38 @@ import json
 
 #variables
 #functions
+def instructs():
+
+    import json
+
+    filename = "instructions.json"
+    with open(filename, 'r') as file:
+        instructions = json.load(file)
+
+        print(instructions["I"])
+        print("")
+        print(instructions["Dl"])
+        print("")
+        print(instructions["1L"])
+        print("")
+        print(instructions["2L"])
+        print("")
+        print(instructions["3L"])
+        print("")
+        print(instructions["4L"])
+        print("")
+
+        print(instructions["Q"])
+        print("")
+        print(instructions["1Q"])
+        print("")
+        print(instructions["2Q"])
+        print("")
+        print(instructions["3Q"])
+        print("")
+        print(instructions["4Q"])
+        print("")
+
 def print_menu_choices():
     filename = "topics.json"
     with open(filename, 'r') as file:
@@ -34,8 +66,10 @@ def developers():
     print("developers placeholder\n")
 #main
 while True:
+    instructs()
     choice = print_menu_choices()
     topics()
     challenges()
     acheivments()
     developers()
+
