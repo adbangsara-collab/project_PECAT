de21d0863445373a0056b3c7ea2554e5cb60928a
jgmacasieb-design <jgmacasieb@carc.pshs.edu.ph>
2026-04-20 11:09:51 +0800

    Update and rename CSprojectbackup to CSprojectbackup.py

--- CSprojectbackup.py
+++ CSprojectbackup.py
@@ -1,5 +1,6 @@
 #imports
 import json
+import time
 
 #variables
 
@@ -63,11 +64,14 @@ def topics(choice): # function for topic option
                             break
                         else:
                             print("enter valid input")
-            for x in data:
-                if x["dict"] == choice1:
-                    print(x[str(choice2)]) # we will make this display text, video or graphic in the future when th files are good
-
-
+        if choice2 == "1":
+            filename = f"topics_{choice1}.json"
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                print(data["greet"])
+                for x in range(1, data["amount"]):
+                    print(data["1"][str(x)], end = "")
+                    sdfdsffdf = input("")
 def challenges(): # function for challenges option
     if choice == "b":
         filename = "challenges.json"
@@ -126,12 +130,13 @@ def challenges(): # function for challenges option
                         if answer.lower() == answers2[(z-1)]:
                             dialog_ans2 = x["dialog_ans2"]
                             print(dialog_ans2["correct"])
+                            correct += 1
                             break
                         else:
                             dialog_ans2 = x["dialog_ans2"]
                             print(dialog_ans2["incorrect"])
                         break
-            if correct == 6:
+            if correct >= 6:
                 print(x["pass"])
             else:
                 print(x["fail"])
@@ -139,16 +144,29 @@ def challenges(): # function for challenges option
         filename = f"acheivments.json"
         with open(filename, 'r', encoding='utf-8') as file:
             data = json.load(file)
-            if correct == 6:
-                data[choice1] = "solved"
-                json.dump(data, file)
+            if correct >= 6:
+                data[f"challenge_{choice1}"] = "solved"
+                print("a")
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+            print("b")
+
 
 
 def acheivments():# function for acheivment option
-    print("achievments placeholder")
-def developers():# function for developers option
-    print("developers placeholder\n")
+    if choice == "c":
+        filename = f"acheivments.json"
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for x in range(6):
+                print(f"challenge {(data["list"])[x]} = {(data[f"challenge_{(data["list"])[x]}"])}")
 
+def developers():# function for developers option
+    if choice == "d":
+        filename = f"developers.json"
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            print(data["developers"])
 #main
 while True:
     instructs()
@@ -157,3 +175,5 @@ while True:
     challenges()
     acheivments()
     developers()
+    if choice == "e":
+        break
